81e17bc2300eebe292bfb36c5e3aefeeec54c4c1
Nachev <n.n.nachev@gmail.com>
2014-02-10 22:12:59 +0200

    Refactoring main test

--- TWVW/TeamWorkVW/Program.cs
+++ TWVW/TeamWorkVW/Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TeamWorkVW
+﻿namespace TeamWorkVW
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             Console.WriteLine("Hello world!");
             Console.WriteLine("Hi there");
