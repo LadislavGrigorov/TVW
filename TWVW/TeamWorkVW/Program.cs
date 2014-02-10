3b16fb973e50d7f83dd9a722763fccf5e52c7867
Ladislav Grigorov <ladislavgrigorov@gmail.com>
2014-02-10 11:34:41 +0200

    test

--- TWVW/TeamWorkVW/Program.cs
+++ TWVW/TeamWorkVW/Program.cs
@@ -11,6 +11,7 @@ namespace TeamWorkVW
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Hi there");
         }
     }
 }
