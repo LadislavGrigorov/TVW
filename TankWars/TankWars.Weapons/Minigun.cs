f54713231383ced8daf4687a835cc3c1f85f5738
Ladislav Grigorov <ladislavgrigorov@gmail.com>
2014-02-25 15:28:57 +0200

    new weapons

--- TankWars/TankWars.Weapons/Minigun.cs
+++ TankWars/TankWars.Weapons/Minigun.cs
@@ -8,8 +8,8 @@ namespace TankWars.Weapons
 {
     public class Minigun : PrimaryWeapon
     {
-        private const int MinigunDamage = 10;
-        private const int MinigunSpeed = 3;
+        private const int MinigunDamage = 80;
+        private const int MinigunSpeed = 6;
 
         public Minigun()
             : base(MinigunDamage, MinigunSpeed)
--- TankWars/TankWars.Weapons/OneShot.cs
+++ TankWars/TankWars.Weapons/OneShot.cs
@@ -8,7 +8,7 @@ namespace TankWars.Weapons
 {
     public class OneShot : SecondaryWeapon
     {
-        private const int OneShotDamage = 100;
+        private const int OneShotDamage = 1000;
         private const int OneshotCharges = 1;
         private const int OneShotSpeed = 1;
         public OneShot() : base(OneShotDamage, OneShotSpeed, OneShotCharges)
