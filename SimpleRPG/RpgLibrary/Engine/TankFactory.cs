345c97c8a0e8a54d89d76b055eab9939ab2ed108
Nachev <n.n.nachev@gmail.com>
2014-02-26 15:34:52 +0200

    New graphical release with XNA

---
+++ SimpleRPG/RpgLibrary/Engine/TankFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace TankWars.Engine
+{
+    using System;
+    using TankWars.Tanks;
+    using TankWars.Common;
+    public class TankFactory
+    {
+        public SwinTank CreateSwimTank(string name, int x, int y)
+        {
+            return new SwinTank(name, new ItemPosition(x, y));
+        }
+
+        public HeavyDutyTank CreateHeavyDutyTank(string name, int x, int y)
+        {
+            return new HeavyDutyTank(name, new ItemPosition(x, y));
+        }
+
+        public StealthTank CreateStealthTank(string name, int x, int y)
+        {
+            return new StealthTank(name, new ItemPosition(x, y));
+        }
+    }
+}
