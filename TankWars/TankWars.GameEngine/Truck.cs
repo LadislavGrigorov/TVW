59901fde9b81f5397c0253803d714562661bc9a7
stoianpp <stoianpp2013@gmail.com>
2014-02-26 19:04:26 +0200

    cars and trucks

---
+++ TankWars/TankWars.GameEngine/Truck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankWars.Common
+{
+    public class Truck: Machine
+    {
+        public Trucks(string name, ItemPosition position) : base(name, position) { }
+    }
+}
