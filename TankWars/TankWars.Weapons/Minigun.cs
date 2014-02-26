956af90811b4366137f025f9d30b32c7300dd300
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-02-26 15:02:20 +0200

    refactoring some using statements

--- TankWars/TankWars.Weapons/Minigun.cs
+++ TankWars/TankWars.Weapons/Minigun.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TankWars.Weapons
+﻿namespace TankWars.Weapons
 {
+
     public class Minigun : PrimaryWeapon
     {
         private const int MinigunDamage = 80;
